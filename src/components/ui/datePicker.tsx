e142d929f3f33aa8a84500cd088e382536c4bcae
Matan Presberg <mgp64@cornell.edu>
2024-06-22 13:23:49 -0700

    popover and calendar components, delete middleware

---
+++ src/components/ui/datePicker.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import * as React from "react";
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
+
+import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
+import { Calendar } from "~/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+
+interface DatePickerProps {
+  onChange: (date: Date) => void;
+}
+
+// export function DatePicker() {
+export const DatePicker: React.FC<DatePickerProps> = ({ onChange }) => {
+  const [date, setDate] = React.useState<Date>();
+
+  React.useEffect(() => {
+    if (date) {
+      onChange(date);
+    }
+  }, [date]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[240px] justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
