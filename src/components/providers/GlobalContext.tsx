1d5826a014fd12235d0f7493958b07e05791eed1
Matan Presberg <mgp64@cornell.edu>
2024-08-20 16:54:01 -0700

    set acting permissions to user privilege level

--- src/components/providers/GlobalContext.tsx
+++ src/components/providers/GlobalContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PrivilegeLevel } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import * as React from "react";
 
 type GlobalContextType = {
@@ -16,8 +17,17 @@ export const GlobalContext = React.createContext<GlobalContextType>({
 });
 
 export function GlobalContextProvider({ children }: React.PropsWithChildren) {
+  const { data: session, status } = useSession();
+
   const [actingPermissions, setActingPermissions] =
     React.useState<PrivilegeLevel>(PrivilegeLevel.ReadAccess);
+
+  React.useEffect(() => {
+    if (status === "authenticated" && session.user?.privilegeLevel) {
+      const privilegeLevel = session.user.privilegeLevel;
+      setActingPermissions(privilegeLevel);
+    }
+  }, [status, session]);
 
   return (
     <GlobalContext.Provider value={{ actingPermissions, setActingPermissions }}>
