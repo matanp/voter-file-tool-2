ce14e338aa479436e9046a9a63f7e3e485aa2ca4
Matan Presberg <mgp64@cornell.edu>
2024-12-10 18:06:14 -0800

    allow committee bulk uploading, and add discrepancy checking plus display

---
+++ apps/frontend/src/app/admin/data/UploadCommitteeButton.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { VoterRecord } from "@prisma/client";
+import { useState } from "react";
+import { Discrepancy } from "~/app/api/lib/utils";
+import { VoterRecordTable } from "~/app/recordsearch/VoterRecordTable";
+import { Button } from "~/components/ui/button";
+
+const discrepanciesPrintMap = {
+  Add1: "Address",
+  "res state": "State",
+  Zip: "Zip Code",
+  City: "City",
+  firstname: "First Name",
+  lastname: "Last Name",
+};
+
+export const UploadCommittee: React.FC = () => {
+  const [isUploading, setIsUploading] = useState(false);
+  const [recordsWithDiscrepancies, setRecordsWithDiscrepancies] = useState<
+    VoterRecord[]
+  >([]);
+  const [discrepanciesMap, setDiscrepanciesMap] = useState<
+    Record<string, Discrepancy>
+  >({});
+
+  //   const handleUploadCommittee = async (
+  //     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  //   ) => {
+  //     e.preventDefault();
+
+  //     const input = document.createElement("input");
+  //     input.type = "file";
+  //     input.accept = ".xlsx";
+
+  //     input.onchange = async (event) => {
+  //       const file = (event.target as HTMLInputElement).files?.[0];
+  //       if (!file) return;
+
+  //       // 5MB limit
+  //       if (file.size > 5 * 1024 * 1024) {
+  //         toast({
+  //           title: "Error",
+  //           description: "File too large. Maximum size is 5MB.",
+  //         });
+  //         return;
+  //       }
+
+  //       const formData = new FormData();
+  //       formData.append("file", file);
+
+  //       try {
+  //         setIsUploading(true);
+  //         const response = await fetch("/api/committees/upload", {
+  //           method: "POST",
+  //           body: formData,
+  //         });
+
+  //         if (!response.ok) {
+  //           const errorData = (await response.json()) as { message: string };
+  //           toast({
+  //             title: "Error",
+  //             description: errorData.message || "Upload failed",
+  //           });
+  //           return;
+  //         }
+
+  //         // const result = await response.json();
+  //         toast({ title: "Success", description: "Uploaded" });
+  //       } catch (error) {
+  //         console.error("Upload error:", error);
+  //         toast({ title: "Error", description: "An unexpected error occurred" });
+  //       } finally {
+  //         setIsUploading(false);
+  //         document.body.removeChild(input);
+  //       }
+  //     };
+
+  //     input.oncancel = () => {
+  //       document.body.removeChild(input);
+  //     };
+
+  //     input.click();
+  //   };
+
+  const handleUploadCommittee = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    setIsUploading(true);
+
+    try {
+      const response = await fetch("/api/committee/bulkLoad", {
+        method: "POST",
+      });
+
+      const data = await response.json();
+
+      setRecordsWithDiscrepancies(data.recordsWithDiscrepancies);
+
+      const discrepanciesRecord: Record<string, Discrepancy> =
+        Object.fromEntries(data.discrepanciesMap);
+
+      setDiscrepanciesMap(discrepanciesRecord);
+    } catch (error) {
+      console.error("Error uploading committee list:", error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={handleUploadCommittee} disabled={isUploading}>
+        {isUploading ? "Uploading..." : "Upload bulk committee list"}
+      </Button>
+      {recordsWithDiscrepancies.length > 0 && (
+        <div className="mt-4">
+          <h2>Records with discrepancies</h2>
+          <VoterRecordTable
+            records={recordsWithDiscrepancies}
+            fieldsList={[]}
+            paginated={false}
+            extraContent={(record: VoterRecord) => {
+              const discrepancies = discrepanciesMap[record.VRCNUM];
+
+              console.log(discrepancies);
+
+              if (!discrepancies) {
+                return <p>No discrepancies found</p>;
+              }
+
+              return (
+                <div className="flex gap-2 items-center">
+                  <ul>
+                    {Object.keys(discrepancies).map((key) => {
+                      const value = discrepancies[key];
+                      return (
+                        <li key={key}>
+                          {
+                            discrepanciesPrintMap[
+                              key as keyof typeof discrepanciesPrintMap
+                            ]
+                          }
+                          : {value?.incoming} vs {value?.existing}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              );
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ apps/frontend/src/app/api/committee/bulkLoad/route.ts
@@ -0,0 +1,34 @@
+import prisma from "~/lib/prisma";
+import { loadCommitteeLists } from "../../bulkLoadData/route";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const discrepanciesMap = await loadCommitteeLists();
+
+    const recordsWithDiscrepancies = await prisma.voterRecord.findMany({
+      where: {
+        OR: Array.from(discrepanciesMap.entries()).map(([key, value]) => ({
+          VRCNUM: key,
+        })),
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Committee lists loaded successfully",
+        discrepanciesMap: Array.from(discrepanciesMap.entries()),
+        recordsWithDiscrepancies,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Error loading committee lists:", error);
+
+    return NextResponse.json(
+      { error: "Error loading committee lists" },
+      { status: 500 },
+    );
+  }
+}
