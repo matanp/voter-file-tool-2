5cb1a2a5b03063c93986b88e12aecfc17fa4c2e7
Matan Presberg <mgp64@cornell.edu>
2024-12-10 18:06:14 -0800

    allow committee bulk uploading, and add discrepancy checking plus display

--- apps/frontend/src/app/components/header.tsx
+++ apps/frontend/src/app/components/header.tsx
@@ -1,17 +1,27 @@
 "use client";
+import { PrivilegeLevel } from "@prisma/client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "~/components/ui/button";
 import { SignInButton } from "~/components/ui/signInButton";
+import { hasPermissionFor } from "~/lib/utils";
 
 const Header: React.FC = () => {
   const pathname = usePathname();
+  const { data: session, status } = useSession();
+
+  const showDataTab = hasPermissionFor(
+    session?.user?.privilegeLevel ?? PrivilegeLevel.ReadAccess,
+    PrivilegeLevel.Developer,
+  );
+
   const sharedTabStyle =
     "h-16 text-xl font-semibold w-42 flex items-center justify-center";
   const tabStyleActive = "bg-primary";
   const tabStyleInactive = "bg-muted-foreground";
   return (
-    <div className="grid grid-cols-header border-bottom m-1 border-2 border-solid bg-accent">
+    <div className="grid grid-cols-header border-bottom p-1 border-2 border-solid bg-accent">
       <div></div>
       <div className="flex-gap flex items-center justify-center gap-2 py-2">
         {/* <ThemeToggle className="mr-auto" /> */}
@@ -36,6 +46,15 @@ const Header: React.FC = () => {
             Reports
           </Button>
         </Link>
+        {showDataTab && (
+          <Link href="admin/data">
+            <Button
+              className={`${sharedTabStyle} ${pathname?.endsWith("admin/data") ? tabStyleActive : tabStyleInactive}`}
+            >
+              Data
+            </Button>
+          </Link>
+        )}
       </div>
       <div className="flex-gap flex items-center justify-center gap-2 py-2">
         <SignInButton />
