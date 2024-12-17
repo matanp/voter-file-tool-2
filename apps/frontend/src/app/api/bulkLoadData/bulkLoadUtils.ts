bb551763b5bb2ef6b559eba498373d7b58ae573b
Matan Presberg <mgp64@cornell.edu>
2024-12-17 14:04:53 -0800

    move loadCommitteeList function to its own file and cleanup some unused code in the route file

---
+++ apps/frontend/src/app/api/bulkLoadData/bulkLoadUtils.ts
@@ -0,0 +1,154 @@
+import { createReadStream } from "fs";
+import prisma from "~/lib/prisma";
+import csv from "csv-parser";
+import * as xlsx from "xlsx";
+import * as fs from "fs";
+import type { Prisma, VoterRecordArchive } from "@prisma/client";
+import {
+  convertStringToDateTime,
+  DiscrepanciesAndCommittee,
+  type DropdownItem,
+  dropdownItems,
+  exampleVoterRecord,
+  fieldEnum,
+  findDiscrepancies,
+  isRecordNewer,
+} from "../lib/utils";
+import { NextResponse } from "next/server";
+
+const committeeData = new Map<
+  string,
+  { data: Prisma.CommitteeListCreateManyInput; committeeMembers: string[] }
+>();
+
+export async function loadCommitteeLists() {
+  const filePath = "data/DemocraticCommitteeExport.xlsx";
+
+  const fileBuffer = fs.readFileSync(filePath);
+  const workbook: xlsx.WorkBook = xlsx.read(fileBuffer);
+
+  const committeeExportSheet: xlsx.WorkSheet | undefined =
+    workbook.Sheets.Export_to_Excel;
+
+  if (!committeeExportSheet) {
+    throw new Error("Committee export sheet not found");
+  }
+
+  const unkownCommitteeData: unknown[] =
+    xlsx.utils.sheet_to_json(committeeExportSheet);
+
+  const committeeExportData = unkownCommitteeData as Record<string, string>[];
+
+  let count = 0;
+  let found = 0;
+  let foundDiscrepancy = 0;
+  const discrepanciesMap = new Map<string, DiscrepanciesAndCommittee>();
+
+  for (const row of committeeExportData) {
+    let city = row["LT Description"]?.includes("City")
+      ? "Rochester"
+      : row["LT Description"];
+
+    city = city?.toUpperCase();
+
+    const legDistrict = Number(row.LT);
+    const electionDistrict = Number(row.ED);
+
+    const VRCNUM = row["voter id"];
+
+    if (!VRCNUM) {
+      throw new Error("VRCNUM is undefined");
+    }
+
+    if (!city || !legDistrict || !electionDistrict) {
+      throw new Error("Invalid committee data");
+    }
+
+    const existingRecord = await prisma.voterRecord.findUnique({
+      where: {
+        VRCNUM,
+      },
+    });
+
+    count++;
+    let recordHasDiscrepancies = false;
+    if (existingRecord) {
+      found++;
+      const discrepancies = findDiscrepancies(row, existingRecord);
+
+      if (discrepancies && Object.keys(discrepancies).length > 0) {
+        discrepanciesMap.set(VRCNUM, {
+          discrepancies,
+          committee: {
+            id: 0,
+            cityTown: city,
+            legDistrict,
+            electionDistrict,
+          },
+        });
+        foundDiscrepancy++;
+        recordHasDiscrepancies = true;
+      }
+    } else {
+      discrepanciesMap.set(VRCNUM, {
+        discrepancies: {
+          VRCNUM: { incoming: VRCNUM, existing: "", fullRow: row },
+        },
+        committee: {
+          id: 0,
+          cityTown: city,
+          legDistrict,
+          electionDistrict,
+        },
+      });
+    }
+
+    const mapKey = `${city}-${legDistrict}-${electionDistrict}`;
+
+    if (!city || !legDistrict || !electionDistrict) {
+      throw new Error("Invalid committee data");
+    }
+
+    if (committeeData.has(mapKey) && !recordHasDiscrepancies) {
+      committeeData.get(mapKey)?.committeeMembers.push(VRCNUM);
+    } else {
+      committeeData.set(mapKey, {
+        data: {
+          cityTown: city,
+          legDistrict: legDistrict,
+          electionDistrict: electionDistrict,
+        },
+        committeeMembers: recordHasDiscrepancies ? [] : [VRCNUM],
+      });
+    }
+  }
+
+  for (const [key, value] of committeeData.entries()) {
+    const committeeList = value.data;
+
+    const committee = await prisma.committeeList.upsert({
+      where: {
+        cityTown_legDistrict_electionDistrict: {
+          cityTown: committeeList.cityTown,
+          legDistrict: committeeList.legDistrict,
+          electionDistrict: committeeList.electionDistrict,
+        },
+      },
+      create: committeeList,
+      update: committeeList,
+    });
+
+    await prisma.voterRecord.updateMany({
+      where: {
+        VRCNUM: {
+          in: value.committeeMembers,
+        },
+      },
+      data: {
+        committeeId: committee.id,
+      },
+    });
+  }
+
+  return discrepanciesMap;
+}
