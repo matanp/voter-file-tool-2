858c1fbe9c6da8d193bf9791ddf1a5eb682a32b2
Matan Presberg <mgp64@cornell.edu>
2024-12-28 15:24:51 -0500

    remove console statements in posthog provider

--- apps/frontend/src/components/providers/PostHog.tsx
+++ apps/frontend/src/components/providers/PostHog.tsx
@@ -5,17 +5,12 @@ import { PostHogProvider } from "posthog-js/react";
 
 type PostHogProviderProps = React.ComponentProps<typeof PostHogProvider>;
 
-console.log("posthog tsx", process.env.NEXT_PUBLIC_POSTHOG_KEY);
-
 if (typeof window !== "undefined") {
-  console.log("initializing posthog", process.env.NEXT_PUBLIC_POSTHOG_KEY);
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "identified_only",
   });
 }
 export function CSPostHogProvider({ children }: PostHogProviderProps) {
-  console.log("posthog provider", process.env.NEXT_PUBLIC_POSTHOG_KEY);
-
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
