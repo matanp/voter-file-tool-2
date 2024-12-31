1f0c23f31f2004370188fa2cb91f13e2b958c956
Matan Presberg <mgp64@cornell.edu>
2024-12-30 19:25:50 -0500

    capture pageviews manually for posthog

--- apps/frontend/src/components/providers/PostHog.tsx
+++ apps/frontend/src/components/providers/PostHog.tsx
@@ -5,6 +5,12 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 
+import dynamic from "next/dynamic";
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+});
+
 type PostHogProviderProps = React.ComponentProps<typeof PostHogProvider>;
 
 if (typeof window !== "undefined") {
@@ -12,6 +18,8 @@ if (typeof window !== "undefined") {
     api_host: "/ingest",
     ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: "always",
+    capture_pageview: false,
+    capture_pageleave: true,
   });
 }
 export function CSPostHogProvider({ children }: PostHogProviderProps) {
@@ -19,14 +27,28 @@ export function CSPostHogProvider({ children }: PostHogProviderProps) {
 
   useEffect(() => {
     if (status === "authenticated" && session.user?.id) {
-      console.log("Identifying user", session.user.id);
+      const name =
+        process.env.NODE_ENV === "development"
+          ? `DEV - ${session.user.name}`
+          : session.user.name;
+
+      const email =
+        process.env.NODE_ENV === "development"
+          ? `DEV - ${session.user.email}`
+          : session.user.email;
+
       posthog.identify(session.user.id, {
-        email: session.user.email,
-        name: session.user.name,
+        name,
+        email,
         privilegeLevel: session.privilegeLevel,
       });
     }
   }, [session, status]);
 
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  return (
+    <PostHogProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PostHogProvider>
+  );
 }
