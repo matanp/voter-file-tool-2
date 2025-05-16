97df4807b8c7d3ea09822c73e6faa170fa0b4af3
Matan Presberg <mgp64@cornell.edu>
2025-05-15 18:32:58 -0700

    refactor some of backend index file into utils file

---
+++ apps/report-server/src/utils.ts
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import PetitionForm from './components/DesignatingPetition';
+import puppeteer from 'puppeteer';
+
+export const generateHTML = (
+  candidates: { name: string; address: string; office: string }[],
+  vacancyAppointments: { name: string; address: string }[],
+  party: string,
+  electionDate: string,
+  numPages: number
+) => {
+  // Make sure the path to your built Tailwind CSS is correct
+  const tailwindCSS =
+    '<link href="http://localhost:8080/tailwind.css" rel="stylesheet">';
+  const html = Array.from({ length: numPages }, (_, i) => i + 1)
+    .map((sheetNum) =>
+      ReactDOMServer.renderToStaticMarkup(
+        React.createElement(PetitionForm, {
+          sheetNumber: sheetNum,
+          candidates: candidates,
+          vacancyAppointments: vacancyAppointments,
+          party: party,
+          electionDate: electionDate,
+        })
+      )
+    )
+    .join('');
+  return `<!DOCTYPE html>
+      <html>
+        <head>
+          <title>PDF Content</title>
+          ${tailwindCSS}
+        </head>
+        <body>
+          ${html}
+        </body>
+      </html>`;
+};
+
+export async function generatePDF(htmlContent: string): Promise<Buffer> {
+  const puppeteerOptions = {
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  };
+
+  const browser = await puppeteer.launch(puppeteerOptions);
+  const page = await browser.newPage();
+
+  await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+  const pdfBuffer = await page.pdf({
+    format: 'Legal',
+    printBackground: true,
+  });
+
+  const buffer = Buffer.from(pdfBuffer);
+
+  // for debugging with the puppeetteer launched
+  // await sleep(999999);
+  // setTimeout(() => {
+  //   browser.close();
+  // }, 10000);
+  await browser.close();
+
+  return buffer;
+}
+
+function sleep(arg0: number) {
+  return new Promise((resolve) => setTimeout(resolve, arg0));
+}
