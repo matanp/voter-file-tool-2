8fdbd4d5f228de93497fd570e80cb819e6f753cd
Matan Presberg <mgp64@cornell.edu>
2025-08-28 22:28:05 -0700

    remove account permissions file

--- apps/frontend/accountPermissions.ts
+++
@@ -1,8 +0,0 @@
-export const accountPermissions = {
-  developer: [
-    "avi.presberg@gmail.com",
-    "mpresberg@gmail.com",
-    "stephen.g.devay@gmail.com",
-    "csardella01@gmail.com",
-  ],
-};
