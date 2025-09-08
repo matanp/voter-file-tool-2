b59ffcb62533b91b6cee1d53eec6a3b21f78b0f3
Matan Presberg <mgp64@cornell.edu>
2025-09-08 12:13:14 -0700

    construct R2 endpoint dynamically instead of an env variable

--- apps/report-server/src/s3Utils.ts
+++ apps/report-server/src/s3Utils.ts
@@ -9,9 +9,11 @@ import { config } from 'dotenv';
 
 config();
 
+const R2_ENDPOINT = `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`;
+
 const s3 = new S3Client({
   region: 'auto', // Cloudflare R2 doesn't care about region
-  endpoint: process.env.R2_ENDPOINT,
+  endpoint: R2_ENDPOINT,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID!,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!,
