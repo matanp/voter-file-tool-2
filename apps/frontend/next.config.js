6474ea933e23b69751266ab77b21b0c16e30c844
Matan Presberg <mgp64@cornell.edu>
2025-09-10 22:10:18 -0700

    deploy a page at the root path

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -18,11 +18,6 @@ const nextConfig = {
   output: "standalone" /* for docker hosting */,
   reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   redirects: async () => {
     return [
       {
