e78eb836f5e26c1b2410d5ed95ee5a529ae66244
Matan Presberg <mgp64@cornell.edu>
2025-10-01 21:18:08 -0700

    Home page (#73)
    add a home page

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+import { HeroSection } from "~/components/home/HeroSection";
+import { FeaturesGrid } from "~/components/home/FeaturesGrid";
+import { KeyCapabilities } from "~/components/home/KeyCapabilities";
+import { QuickActions } from "~/components/home/QuickActions";
+import { Footer } from "~/components/home/Footer";
+
+/**
+ * Main homepage component that composes all sections
+ */
+export default function HomePage() {
+  return (
+    <div className="w-full min-h-screen bg-background">
+      <div className="container mx-auto px-4 py-16">
+        <HeroSection />
+        <FeaturesGrid />
+        {/* <KeyCapabilities /> */}
+        <QuickActions />
+        <Footer />
+      </div>
+    </div>
+  );
+}
